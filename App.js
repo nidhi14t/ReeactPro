a5d35862e72151e5e5f7a140af325f439974465a
Nidhi Tiwari <nidhi.14t@gmail.com>
2023-06-17 21:25:00 -0400

    Added files for React simple code

---
+++ App.js
@@ -0,0 +1,32 @@
+const heading = React.createElement("div", { id: "rootDiv", abra: "nidhi" }, [
+  React.createElement("div", { id: "child1", abra: "nidhi" }, [
+    React.createElement("div", { id: "child2", abra: "nidhi" }, [
+      React.createElement(
+        "h1",
+        { id: "h1", abra: "nidhi" },
+        "Hello I am Nidhi child 1 heading 1"
+      ),
+      React.createElement(
+        "h2",
+        { id: "h2", abra: "nidhi" },
+        "Hello I am Nidhi child 1 heading 2"
+      ),
+    ]),
+    React.createElement("div", { id: "child2", abra: "nidhi" }, [
+      React.createElement(
+        "h1",
+        { id: "h1", abra: "nidhi" },
+        "Hello I am Nidhi child 2 heading 1"
+      ),
+      React.createElement(
+        "h2",
+        { id: "h2", abra: "nidhi" },
+        "Hello I am Nidhi child 2 heading 2"
+      ),
+    ]),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
