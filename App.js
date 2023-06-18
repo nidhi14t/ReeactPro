c3ed5e1a1b4dffa4f9119d700a6f653e44e8fdc5
Nidhi Tiwari <nidhi.14t@gmail.com>
2023-06-18 15:15:41 -0400

    React Element and Component Intro

--- App.js
+++ App.js
@@ -1,35 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "rootDiv", abra: "nidhi" }, [
-  React.createElement("div", { id: "child1", abra: "nidhi" }, [
-    React.createElement("div", { id: "child2", abra: "nidhi" }, [
-      React.createElement(
-        "h1",
-        { id: "h1", abra: "nidhi" },
-        "Hello I am Nidhi child 1 yo 1"
-      ),
-      React.createElement(
-        "h2",
-        { id: "h2", abra: "nidhi" },
-        "Hello I am Nidhi child 1 heading 2"
-      ),
-    ]),
-    React.createElement("div", { id: "child2", abra: "nidhi" }, [
-      React.createElement(
-        "h1",
-        { id: "h1", abra: "nidhi" },
-        "Hello I am Nidhi child 2 heading 1"
-      ),
-      React.createElement(
-        "h2",
-        { id: "h2", abra: "nidhi" },
-        "Hello I am Nidhi child 2 heading 2"
-      ),
-    ]),
-  ]),
-]);
+const TitleComponent = () => {
+  return <h1 className="title1">Heading Title</h1>;
+};
+
+const TitleComponentFunc = function () {
+  return <h1 className="title1">Heading Title</h1>;
+};
+
+// React Component
+const HeadingComponent = () => (
+  // React Element
+  <div id="container">
+    <TitleComponent />
+    <h1 className="heading" tabIndex="5">
+      I am Jsx heading
+    </h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
