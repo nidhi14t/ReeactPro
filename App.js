bde903bdefc3ef352f5fb60366467258afc5972e
Nidhi Tiwari <nidhi.14t@gmail.com>
2023-06-19 13:19:08 -0400

    Playing around

--- App.js
+++ App.js
@@ -1,25 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const TitleComponent = () => {
-  return <h1 className="title1">Heading Title</h1>;
-};
-
-const TitleComponentFunc = function () {
-  return <h1 className="title1">Heading Title</h1>;
-};
-
 // React Component
 const HeadingComponent = () => (
   // React Element
   <div id="container">
-    <TitleComponent />
     <h1 className="heading" tabIndex="5">
       I am Jsx heading
     </h1>
   </div>
 );
 
+const elem = <span>I am an element</span>;
+const title = (
+  <>
+    <div>
+      <h1 className="title1" tabIndex="5">
+        {elem}
+        Heading Title
+      </h1>
+      <HeadingComponent />
+    </div>
+    <div>
+      <h1 className="title1" tabIndex="5">
+        {elem}
+        Heading Title
+      </h1>
+      <HeadingComponent />
+    </div>
+  </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(title);
